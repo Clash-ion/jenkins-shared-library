13125ad44bd9ebb2ab47a1ec1873a6401daa9191
Mukul Kumar <mukul0000kumar@gmail.com>
2022-07-22 23:28:37 +0530

    change somethin=g

--- vars/makeImage.groovy
+++ vars/makeImage.groovy
@@ -2,8 +2,8 @@ import com.example.Docker
 
 def call(String imageName) {
     echo 'building image ...'
-    def docker
-    docker = new Docker(this)
+    
+    Docker docker = new Docker(this)
     docker.dockerBuild(imageName)
     docker.dockerLogin()
     docker.dockerPush(imageName)
